7c91f3c8bb72404dcdab0cb8f177801fd79e7d20
davemf_ <davemaulanaferros@gmail.com>
2025-08-04 17:49:39 +0700

    update abi unused

--- Context/contants.js
+++ Context/contants.js
@@ -34,26 +34,3 @@ export const CONTRACTS = {
     }
   };
 
-
-//Optimized ✅
-// export const CrowdFundingAddress = "0x8FA5fD1381877C6A3F24B181ADbdD579865e7585";
-// export { default as CrowdFundingABI } from "./CrowdFunding.json";
-
-//Not-Optimized ✅
-// export const CrowdFundingAddress = "0x205EC3cc793Ff3290B0B8507972a5B5532AbE208";
-// export const CrowdFundingABI = crowdFunding.abi;
-
-//PackingOnly ✅
-// export const CrowdFundingAddress = "0x46b5C01774f223b1249A06788B101c7B6eE77bc5";
-// export { default as CrowdFundingABI } from "./CrowdFunding.json";
-
-//BatchOnly ✅
-// export const CrowdFundingAddress = "0xbD76b9fe30E2E0A786367CF5f9b49F6E38396176";
-// export const CrowdFundingABI = crowdFunding.abi;
-
-
-
-//PackingOnly Dpsk
-
-
-//Optimized Dpsk
